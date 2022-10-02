79db6fd762c99a89abe2718943b8d49d2b6094cf
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2022-10-01 23:26:25 -0300

    Update ex012.js

--- Curso/ex012.js
+++ Curso/ex012.js
@@ -4,7 +4,7 @@ var minuto = agora.getMinutes()
 console.log(`Agora são exatamente ${hora} horas.`)
 if (hora < 12) {
     console.log('Bom dia!')
-} else if (hora <= 18) {
+} else if (hora < 19) {
     console.log('Boa tarde!')
 } else {
     console.log('Boa noite!')
